fc437c528d848e2269bd9f81be7c7d9a1466591e
Julian Romero <master7n7@gmail.com>
2024-01-22 07:53:42 -0500

    Started repository

---
+++ src/main/java/com/springdrones/ecommerce/SpringDronecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.springdrones.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDronecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDronecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdrones/ecommerce/SpringDronecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdrones.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDronecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
