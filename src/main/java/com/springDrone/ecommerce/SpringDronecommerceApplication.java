5948c79f34074e8c9dd26360017d710eb2bc1fda
Julian Romero <master7n7@gmail.com>
2024-01-22 18:26:00 -0500

    Fixed dependency problems and created project structure.

--- src/main/java/com/springDrone/ecommerce/SpringDronecommerceApplication.java
+++ src/main/java/com/springDrone/ecommerce/SpringDronecommerceApplication.java
@@ -1,4 +1,4 @@
-package com.springdrones.ecommerce;
+package com.springDrone.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
