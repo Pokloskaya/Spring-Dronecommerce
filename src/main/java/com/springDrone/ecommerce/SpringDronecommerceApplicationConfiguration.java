a465da8942d4b6880b3595d16a17e0163eb72a47
Julian Romero <master7n7@gmail.com>
2024-01-25 15:40:32 -0500

    Implementation: of fragments

---
+++ src/main/java/com/springDrone/ecommerce/SpringDronecommerceApplicationConfiguration.java
@@ -0,0 +1,19 @@
+package com.springDrone.ecommerce;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class SpringDronecommerceApplicationConfiguration {
+    @Bean
+    public ITemplateResolver svgTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg/");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+
+        return resolver;
+    }
+}
