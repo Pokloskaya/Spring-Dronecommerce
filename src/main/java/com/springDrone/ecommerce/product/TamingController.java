26a5260c2376d679b883849f469adcde32f02451
Julian Romero <master7n7@gmail.com>
2024-01-30 19:35:44 -0500

    Deleted unused files

--- src/main/java/com/springDrone/ecommerce/product/TamingController.java
+++ src/main/java/com/springDrone/ecommerce/product/TamingController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProductController {
+public class TamingController {
 
-    @GetMapping("/products")
+    @GetMapping("/taming")
     public String products() {
-        return "/product/index";
+        return "/taming/index";
     }
     
 }
